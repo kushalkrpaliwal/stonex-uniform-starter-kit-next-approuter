6affa14d723868b2fdaa4131dad478a6110fa524
Michal Majer <Michal.Majer@StoneX.com>
2024-06-20 16:19:22 +0200

    tweak image wiedth and height for no object-fit

--- src/canvas/Image/Image.tsx
+++ src/canvas/Image/Image.tsx
@@ -52,7 +52,7 @@ export const Image: FC<ImageProps> = ({
     <div
       className={classNames(
         'relative',
-        { 'w-full f-full': !objectFit || objectFit === 'none' },
+        { 'w-full f-full': objectFit === 'cover' || objectFit === 'contain' || objectFit === 'fill' },
         getBorderColorStyle(borderColor),
         getBorderRadiusStyle(borderRadius)
       )}
